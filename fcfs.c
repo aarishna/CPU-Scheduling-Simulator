cff35df0d5627560a6720ec199c4f001e01e08e9
Aarish Naiyer <aarishnaiyer@gmail.com>
2026-01-04 01:14:26 -0700

    FCFS scheduler

--- fcfs.c
+++ fcfs.c
@@ -1,42 +1,38 @@
-#include <stdio.h>
-#define max 10
+    #include "scheduler.h"
+    #include <string.h>
 
 
-void fcfs(int bt[], int n){
-    int waiting_time=0;
-    int turnaround_time=0;
-    int total=0;
-    
-    printf("\nProcess No.\tWaiting Time\tTurnaround Time\n");
-    
-    for (int i = 0; i < n; i++)
-    {
-        total += bt[i];
+    int schedule_fcfs(Process *procs, int n, TimelineEvent *timeline) {
+        int current_time = 0;     
+        int timeline_idx = 0;     // Track Gantt chart events
         
-        printf("%d\t\t%d\t\t%d\n", (i + 1), (total - bt[i]), total);
+        for (int i = 0; i < n; i++) {
+            // Handle idle time (if process hasn't arrived yet)
+            if (current_time < procs[i].arrival_time) {
+                current_time = procs[i].arrival_time;
+            }
+            
+            // Record when process first starts
+            if (procs[i].start_time == -1) {
+                procs[i].start_time = current_time;
+            }
         
-        waiting_time += (total - bt[i]);
-        turnaround_time += total;
+            
+            // Create timeline event 
+            timeline[timeline_idx].pid = procs[i].pid;
+            timeline[timeline_idx].start = current_time;
+            timeline[timeline_idx].end = current_time + procs[i].burst_time;
+            strcpy(timeline[timeline_idx].state, "running");
+            timeline_idx++;
+            
+
+            current_time += procs[i].burst_time;
+            
+            // Record completion time 
+            procs[i].completion_time = current_time;
+            
+        
+        }
+        
+        return timeline_idx;
     }
-    printf("\n");
-    
-
-}
-int main(){
-
-    int bt[max], n;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-    
-    printf("Enter the burst times of each process:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter the burst time of process %d: ", i+1);
-        scanf("%d", &bt[i]);
-        
-    }
-    
-    fcfs(bt, n);
-
-
-}
